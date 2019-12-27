8b8305a4328ba768459b0996b5119d6aad66f5be
hetengfei <2889921390@qq.com>
2019-12-27 10:14:26 +0800

    初始化项目结构

---
+++ gmall-admin-web/src/test/java/com/baizhi/gmall/admin/GmallAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.gmall.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
